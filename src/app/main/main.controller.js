9a82f203963c40b15c66fa10a0a1b77cf3cb2934
Marcin <mkoczorowski@n-educatio.pl>
2017-02-09 14:25:24 +0100

    added get score button with functionality

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -7,12 +7,22 @@
 
   /** @ngInject */
   function MainController($scope, Item, GetJson, ExerciseModel) {
+    $scope.showProgress = 0;
     $scope.checkAnswers = function() {
       console.log(ExerciseModel.evaluateItems());
-    }
+    };
 
     $scope.reset = function() {
       console.log(ExerciseModel.resetValues());
+    };
+
+    $scope.getScore = function() {
+      console.log(ExerciseModel.getScore());
+      console.log(ExerciseModel.getSum());
+      $scope.showProgress = ExerciseModel.getSum();
+      if ($scope.showProgress === 5) {
+        console.log("kuniec");
+      }
     }
   }
 
--- src/app/main/models/exercise.model.js
+++ src/app/main/models/exercise.model.js
@@ -8,14 +8,22 @@
   /** @ngInject */
   function ExerciseModelService() {
     var _list = [];
+    var sum = 0;
     return {
+
       getList: function() {
         return _list;
       },
+
+      getSum:function() {
+        return sum;
+      },
+
       addPeople: function(model) {
         return _list.push(model);
         console.log(_list);
       },
+
       evaluateItems: function() {
         _list.forEach(function(element) {
           if (element.answer.indexOf(element.inputValue) !== -1) {
@@ -28,6 +36,7 @@
         });
         return _list;
       },
+
       resetValues: function() {
         _list.forEach(function(element) {
           if (element.answer.length > 0) {
@@ -37,7 +46,18 @@
           }
         });
         return _list;
+      },
+
+      getScore: function() {
+        sum=0;
+        _list.forEach(function(element) {
+          if(element.displayOkIconValue === true) {
+            this.getSum(++sum);
+          }
+        }, this);
+        return _list;
       }
+
     }
   }
 })();
