b7e239c0f71893c8adac34ecc4d970338e10fecf
Marcin <mkoczorowski@n-educatio.pl>
2017-02-10 16:43:13 +0100

    work with ui-router

--- src/app/main/routers/route.config.js
+++ src/app/main/routers/route.config.js
@@ -4,12 +4,17 @@
   angular
     .module('cartProject')
     .config(function($stateProvider, $urlRouterProvider) {
-      $urlRouterProvider.otherwise('/#');
+      $urlRouterProvider.otherwise('/task1');
       $stateProvider
-        .state('home', {
-          url: '/',
+        .state('task1', {
+          url: '/task1',
           templateUrl: 'app/main/views/task1.html',
           controller: 'MainController'
-        });
+        })
+        .state('task2', {
+          url: '/task2',
+          templateUrl: 'app/main/views/task2.html',
+          controller: 'MainController'
+        })
     });
 })();
